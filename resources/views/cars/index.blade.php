f8ceb7a7e8b4444b81e0b02c3c542a68f1d26037
Pau Garcia Martínez <etpau@hotmail.es>
2022-02-03 23:25:45 +0100

    Set main layout and migrate cars database

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -1,11 +1,19 @@
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-  <p>Cars</p>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+  <div class="m-auto w-4/5 py-24">
+    <div class="text-center">
+      <h1 class="text-5xl uppercase bold">CARS</h1>
+    </div>
+
+    <div class="w-5/6 py-10">
+      <div class="m-auto">
+        <span class="uppercase text-blue-500 font-bold text-xs italic">Founded: 2022</span>
+        <h2 class="text-gray-700 text-5xl">Audi</h2>
+        <p class="text-lg text-gray-700 py-6">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam pariatur voluptate nisi deleniti! Quo doloribus quae vitae voluptatum? Ea illo sit odio. Sunt quod reprehenderit, soluta eligendi repudiandae molestias neque.</p>
+        <hr class="mt-4 mb-8">
+      </div>
+    </div>
+  </div>
+  
+@endsection
