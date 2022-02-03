7798b84388a4007f405f5663d7fa75cec8f3ec42
Pau Garcia Martínez <etpau@hotmail.es>
2022-02-03 23:58:09 +0100

    Set create view

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
   <div class="m-auto w-4/5 py-24">
+
+    <div class="pt-10">
+      <a href="cars/create" class="border-b2 pb-2 border-dotted italic text-gray-500">Create a new car</a>
+    </div>
+
     <div class="text-center">
       <h1 class="text-5xl uppercase bold">CARS</h1>
     </div>
